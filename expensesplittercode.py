663b7fd99cfd28d69f4ba5dcd1c69e136a797efb
patriciaaramayom <141798867+patriciaaramayom@users.noreply.github.com>
2023-09-10 21:39:26 -0600

    expensesplittercode.py

--- expensesplittercode.py
+++ expensesplittercode.py
@@ -1,4 +1,11 @@
-def create_new_group(): 
+#The menu function shows the options that the user has.
+def menu():
+    print('WELCOME TO EXPENSE SPLITTER')
+    print('	1.New Group')
+    print('	2.Exit')
+
+#This function has the purpose of asking the user basic info about the new expense, this is needed to make calculations in future fuctions.
+def create_new_group():
     title=input('Title: ')
     des=input('Description: ')
     cur=input('Currency: ')
@@ -6,22 +13,27 @@ def create_new_group():
     par=float(input('Participants: '))
     return title,des,cur,cat,par
 
-
-def calc(title,des,cur,cat,par):
-    expense=float(input('Expense: '))
+#This is the fuction that makes the calculations, with the expense and participants value, it will show the amount everyone has to pay, equially divided.
+def calc(title,des,cur,cat,par, expense):
     amount= expense/par
     print (amount)
     return
 
-def menu():
-    print('WELCOME TO EXPENSE SPLITTER')
-    print('	1.New Group')
-    print('	2.Exit')
-    data=int(input("Enter your choice: "))
-    return data
-data=menu()
-if data==1:
-    title,des,cur,cat,par=create_new_group()
-    calc(title,des,cur,cat,par)
-else:
-    print('Goodbye')
+#the main fuction is where we ask the user for the inputs, except for the create_new_group function. In this funcion we use the condicionals and loops. When the user finished using a group, the menu will be shown again.
+def main():
+    continua= True
+    while continua:
+        print("\n")
+        menu()
+        opcion = int(input("Choose an option: "))
+        if opcion == 1:
+            title,des,cur,cat,par=create_new_group()
+            expense=float(input("Put a value: "))
+            calc(title,des,cur,cat,par, expense)
+            
+        elif opcion == 2:
+            print('Goodbye')
+        else:
+            print("Error")
+main()
+    
